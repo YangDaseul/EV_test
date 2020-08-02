22adf573cbe3f4e4a884f7ca08b755ee1ce10265
hjpark <hjpark@ksmartech.com>
2020-08-03 02:09:00 +0900

    프레임워크작업중20  - gif를 webp로 변환 후 동작 테스트

--- app/src/main/java/com/genesis/apps/ui/fragment/main/FragFirst.java
+++ app/src/main/java/com/genesis/apps/ui/fragment/main/FragFirst.java
@@ -1,31 +1,41 @@
 package com.genesis.apps.ui.fragment.main;
 
+import android.content.res.AssetManager;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.integration.webp.decoder.WebpDrawable;
+import com.bumptech.glide.integration.webp.decoder.WebpDrawableTransformation;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 import com.bumptech.glide.request.RequestOptions;
-import com.bumptech.glide.request.target.DrawableImageViewTarget;
+import com.bumptech.glide.request.transition.DrawableCrossFadeTransition;
+import com.bumptech.glide.request.transition.Transition;
+import com.bumptech.glide.request.transition.TransitionFactory;
 import com.genesis.apps.R;
 import com.genesis.apps.comm.model.ExampleReqVO;
-import com.genesis.apps.comm.model.ExampleResVO;
 import com.genesis.apps.comm.model.ExampleViewModel;
-import com.genesis.apps.comm.model.weather.WeatherPointResVO;
-import com.genesis.apps.comm.model.weather.WeatherPointViewModel;
-import com.genesis.apps.comm.net.NetUIResponse;
 import com.genesis.apps.databinding.Frame1pBinding;
 import com.genesis.apps.ui.fragment.SubFragment;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
+
 import androidx.annotation.Nullable;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.ListAdapter;
 
 public class FragFirst extends SubFragment<Frame1pBinding> {
     private ExampleViewModel exampleViewModel;
+
 //    private WeatherPointViewModel weatherPointViewModel;
 
     @Override
@@ -80,9 +90,57 @@ public class FragFirst extends SubFragment<Frame1pBinding> {
 
     @Override
     public void onRefresh() {
-        Glide.with(this).load(R.drawable.snow).into(me.gifImage);
+//        Glide.with(this).load(R.drawable.snow).diskCacheStrategy(DiskCacheStrategy.RESOURCE).into(me.gifImage);
+//.optionalTransform(WebpDrawable.class, new WebpDrawableTransformation(circleCrop))
+        Glide.with(this).load(setupSampleFile()).transition(DrawableTransitionOptions.with(new DrawableAlwaysCrossFadeFactory())) .apply(RequestOptions.diskCacheStrategyOf(DiskCacheStrategy.ALL)).   into(me.gifImage);
 
-
-
+//        Glide.with(this).load(setupSampleFile()).apply(RequestOptions.diskCacheStrategyOf(DiskCacheStrategy.RESOURCE)).into(me.gifImage);
     }
+
+
+    class DrawableAlwaysCrossFadeFactory implements TransitionFactory<Drawable> {
+        private DrawableCrossFadeTransition resourceTransition = new DrawableCrossFadeTransition(2000, true);
+
+        @Override
+        public Transition<Drawable> build(DataSource dataSource, boolean isFirstResource) {
+            return resourceTransition;
+        }
+    }
+
+
+
+    private String setupSampleFile() {
+        AssetManager assetManager = getActivity().getAssets();
+        String srcFile = "sample1.webp";
+        String destFile = getActivity().getFilesDir().getAbsolutePath() + File.separator + srcFile;
+        copyFile(assetManager, srcFile, destFile);
+        return destFile;
+    }
+
+    private void copyFile(AssetManager assetManager, String srcFile, String destFile) {
+        try {
+            InputStream is = assetManager.open(srcFile);
+            FileOutputStream os = new FileOutputStream(destFile);
+
+            byte[] buffer = new byte[1024];
+            int read;
+            while ((read = is.read(buffer)) != -1) {
+                os.write(buffer, 0, read);
+            }
+            is.close();
+            os.flush();
+            os.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
+
+
+
+
 }
