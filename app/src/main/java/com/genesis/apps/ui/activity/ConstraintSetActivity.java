19fb0b278e8fffd59436851672cad9661f396242
hjpark <hjpark@ksmartech.com>
2020-08-28 11:05:05 +0900

    [modify] ConstraintSet - 키패드 위에 버튼 활성화 추가 - 인터렉션 수정

--- app/src/main/java/com/genesis/apps/ui/activity/ConstraintSetActivity.java
+++ app/src/main/java/com/genesis/apps/ui/activity/ConstraintSetActivity.java
@@ -1,39 +1,36 @@
 package com.genesis.apps.ui.activity;
 
 
-import android.app.Service;
-import android.content.Context;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
 import android.view.animation.OvershootInterpolator;
-import android.view.inputmethod.InputMethodManager;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.genesis.apps.R;
+import com.genesis.apps.comm.util.SoftKeyboardUtil;
+import com.genesis.apps.databinding.ActivityCont1Binding;
+import com.google.android.material.textfield.TextInputEditText;
 
 import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.transition.ChangeBounds;
 import androidx.transition.Transition;
 import androidx.transition.TransitionManager;
 
-import com.genesis.apps.R;
-import com.genesis.apps.comm.util.SoftKeyboardUtil;
-import com.genesis.apps.databinding.ActivityCont1Binding;
-
 public class ConstraintSetActivity extends SubActivity<ActivityCont1Binding> {
 
     private final int[] layouts = {R.layout.activity_cont_1, R.layout.activity_cont_2, R.layout.activity_cont_3, R.layout.activity_cont_4};
+    private View[] editText;
+    private View[] editLayout;
     private ConstraintSet[] constraintSets = new ConstraintSet[layouts.length];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(layouts[0]);
-        initConstraintSets();
-        swapView();
-        SoftKeyboardUtil.showKeyboard(this);
+        init();
     }
 
     @Override
@@ -41,86 +38,85 @@ public class ConstraintSetActivity extends SubActivity<ActivityCont1Binding> {
         super.onDestroy();
     }
 
-    private void initConstraintSets(){
-        for(int i=0; i<layouts.length; i++){
+    private void init() {
+        initConstraintSets();
+        initView();
+        SoftKeyboardUtil.showKeyboard(this);
+    }
+
+    private void initConstraintSets() {
+        editText = new View[]{ui.edit1, ui.edit2, ui.edit3, ui.edit4};
+        editLayout = new View[]{ui.lEdit, ui.lEdit2, ui.lEdit3, ui.lEdit4};
+        for (int i = 0; i < layouts.length; i++) {
             constraintSets[i] = new ConstraintSet();
-            if(i==0)
+
+            if (i == 0)
                 constraintSets[i].clone(ui.container);
             else
                 constraintSets[i].clone(this, layouts[i]);
         }
     }
 
-    public void swapView(){
-        ui.edit1.setOnFocusChangeListener(focusChangeListener);
-        ui.edit2.setOnFocusChangeListener(focusChangeListener);
-        ui.edit3.setOnFocusChangeListener(focusChangeListener);
+    public void initView() {
+        ui.edit1.setOnEditorActionListener(editorActionListener);
+        ui.edit2.setOnEditorActionListener(editorActionListener);
+        ui.edit3.setOnEditorActionListener(editorActionListener);
+        ui.edit4.setOnEditorActionListener(editorActionListener);
 
-        ui.edit1.setOnEditorActionListener(new EditText.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent e) {
-                switch (actionId) {
-                    case 0 :
-                        Transition changeBounds = new ChangeBounds();
-                        changeBounds.setInterpolator(new OvershootInterpolator());
-                        TransitionManager.beginDelayedTransition(ui.container);
-                        if(ui.lEdit2.getVisibility()==View.GONE){
-                            constraintSets[1].applyTo(ui.container);
-                            ui.edit1.setOnFocusChangeListener(null);
-                        }
-                        // TODO : process action button "OK!" (100)
-                        System.out.println("OK! action button is pressed.") ;
-                        break ;
+        ui.btn.setOnClickListener(view -> {
+            if(getCurrentFocus() instanceof TextView){
+                if(!TextUtils.isEmpty(((TextView) getCurrentFocus()).getText().toString())){
+                    doTransition(getViewPosition(getCurrentFocus()));
                 }
-
-                return true ;
             }
         });
-
     }
 
-
-    View.OnFocusChangeListener focusChangeListener = new View.OnFocusChangeListener() {
-        @Override
-        public void onFocusChange(View view, boolean b) {
-
-            if(!TextUtils.isEmpty(((TextView)view).getText().toString())) {
-                switch (view.getId()) {
-                    case R.id.edit1:
-
-                        if(ui.lEdit2.getVisibility()==View.GONE){
-                            Transition changeBounds = new ChangeBounds();
-                            changeBounds.setInterpolator(new OvershootInterpolator());
-                            TransitionManager.beginDelayedTransition(ui.container);
-                            constraintSets[1].applyTo(ui.container);
-                            ui.edit1.setOnFocusChangeListener(null);
-                        }
-                        break;
-                    case R.id.edit2:
-                        if(ui.lEdit3.getVisibility()==View.GONE){
-                            Transition changeBounds = new ChangeBounds();
-                            changeBounds.setInterpolator(new OvershootInterpolator());
-                            TransitionManager.beginDelayedTransition(ui.container);
-                            constraintSets[2].applyTo(ui.container);
-                            ui.edit2.setOnFocusChangeListener(null);
-                        }
-                        break;
-                    case R.id.edit3:
-                        if(ui.lEdit4.getVisibility()==View.GONE){
-                            Transition changeBounds = new ChangeBounds();
-                            changeBounds.setInterpolator(new OvershootInterpolator());
-                            TransitionManager.beginDelayedTransition(ui.container);
-                            constraintSets[3].applyTo(ui.container);
-                            ui.edit3.setOnFocusChangeListener(null);
-                        }
-                        break;
-                    case R.id.edit4:
-                        break;
-                }
-
+    EditText.OnEditorActionListener editorActionListener = (textView, actionId, keyEvent) -> {
+        if(actionId==EditorInfo.IME_ACTION_DONE){
+            if (!TextUtils.isEmpty(textView.getText().toString())) {
+                doTransition(getViewPosition(textView));
             }
-
         }
+        return false;
     };
 
-}
+    private void setVisibleView() {
+        for (int i = 0; i < editLayout.length - 1; i++) {
+            if (editLayout[i + 1].getVisibility() == View.GONE) {
+                doTransition(i + 1);
+                break;
+            }
+        }
+    }
+
+    private int getViewPosition(View view){
+        int pos=0;
+        for(int i=0; i<editText.length-1; i++){
+            if(view.getId()==editText[i].getId()){
+                pos = i+1;
+                break;
+            }
+        }
+        return pos;
+    }
+
+    /**
+     * 지정된 포지션에 대한 트랜지션 진행
+     *
+     * @param pos
+     */
+    private void doTransition(int pos) {
+        if (editLayout[pos].getVisibility() == View.GONE) {
+            Transition changeBounds = new ChangeBounds();
+            changeBounds.setInterpolator(new OvershootInterpolator());
+            TransitionManager.beginDelayedTransition(ui.container);
+            constraintSets[pos].applyTo(ui.container);
+
+            if (editText[pos] instanceof TextInputEditText) {
+                editText[pos].requestFocus();
+            }
+        }
+    }
+
+    }
