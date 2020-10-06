a1bc779d17685d0961dfb6fb70230317c6644c52
hjpark <hjpark@ksmartech.com>
2020-10-07 03:16:00 +0900

    [Modify]  - 인트로프로그레스바

--- app/src/main/java/com/genesis/apps/comm/viewmodel/CMNViewModel.java
+++ app/src/main/java/com/genesis/apps/comm/viewmodel/CMNViewModel.java
@@ -1,8 +1,7 @@
 package com.genesis.apps.comm.viewmodel;
 
-import android.text.TextUtils;
-
 import com.genesis.apps.comm.model.constants.KeyNames;
+import com.genesis.apps.comm.model.constants.VariableType;
 import com.genesis.apps.comm.model.gra.api.CMN_0001;
 import com.genesis.apps.comm.model.gra.api.CMN_0002;
 import com.genesis.apps.comm.model.gra.api.CMN_0003;
@@ -10,12 +9,11 @@ import com.genesis.apps.comm.model.gra.api.CMN_0004;
 import com.genesis.apps.comm.model.repo.CMNRepo;
 import com.genesis.apps.comm.model.repo.DBContentsRepository;
 import com.genesis.apps.comm.model.repo.DBGlobalDataRepository;
-import com.genesis.apps.comm.model.vo.CardVO;
 import com.genesis.apps.comm.model.vo.FloatingMenuVO;
+import com.genesis.apps.comm.model.vo.NotiVO;
 import com.genesis.apps.comm.model.vo.QuickMenuVO;
 import com.genesis.apps.comm.model.vo.WeatherVO;
 import com.genesis.apps.comm.net.NetUIResponse;
-import com.genesis.apps.comm.util.PackageUtil;
 import com.genesis.apps.comm.util.excutor.ExecutorService;
 import com.genesis.apps.room.ResultCallback;
 import com.google.common.util.concurrent.FutureCallback;
@@ -23,9 +21,9 @@ import com.google.common.util.concurrent.Futures;
 
 import org.checkerframework.checker.nullness.compatqual.NullableDecl;
 
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 import androidx.hilt.Assisted;
 import androidx.hilt.lifecycle.ViewModelInject;
@@ -38,8 +36,6 @@ import static com.genesis.apps.comm.model.constants.VariableType.MAIN_VEHICLE_TY
 import static com.genesis.apps.comm.model.constants.VariableType.MAIN_VEHICLE_TYPE_CV;
 import static com.genesis.apps.comm.model.constants.VariableType.MAIN_VEHICLE_TYPE_NV;
 import static com.genesis.apps.comm.model.constants.VariableType.MAIN_VEHICLE_TYPE_OV;
-import static com.genesis.apps.comm.model.vo.CardVO.CARD_STATUS_20;
-import static com.genesis.apps.comm.model.vo.CardVO.CARD_STATUS_30;
 
 public @Data
 class CMNViewModel extends ViewModel {
@@ -59,8 +55,7 @@ class CMNViewModel extends ViewModel {
             CMNRepo repository,
             DBGlobalDataRepository dbGlobalDataRepository,
             DBContentsRepository dbContentsRepository,
-            @Assisted SavedStateHandle savedStateHandle)
-    {
+            @Assisted SavedStateHandle savedStateHandle) {
         this.repository = repository;
         this.dbGlobalDataRepository = dbGlobalDataRepository;
         this.dbContentsRepository = dbContentsRepository;
@@ -72,53 +67,57 @@ class CMNViewModel extends ViewModel {
         RES_CMN_0004 = repository.RES_CMN_0004;
     }
 
-    public void reqCMN0001(final CMN_0001.Request reqData){
+    public void reqCMN0001(final CMN_0001.Request reqData) {
         repository.REQ_CMN_0001(reqData);
     }
-    public void reqCMN0002(final CMN_0002.Request reqData){
+
+    public void reqCMN0002(final CMN_0002.Request reqData) {
         repository.REQ_CMN_0002(reqData);
     }
-    public void reqCMN0003(final CMN_0003.Request reqData){
+
+    public void reqCMN0003(final CMN_0003.Request reqData) {
         repository.REQ_CMN_0003(reqData);
     }
-    public void reqCMN0004(final CMN_0004.Request reqData){
+
+    public void reqCMN0004(final CMN_0004.Request reqData) {
         repository.REQ_CMN_0004(reqData);
     }
 
-    public void updateNotiDt(String notiDt){
+    public void updateNotiDt(String notiDt) {
         dbGlobalDataRepository.update(KeyNames.KEY_NAME_DB_GLOBAL_DATA_NOTIDT, notiDt);
     }
 
-    public boolean setWeatherList(List<WeatherVO> list){
+    public boolean setWeatherList(List<WeatherVO> list) {
         return dbContentsRepository.setWeatherList(list);
     }
 
-    public boolean setQuickMenu(List<QuickMenuVO> list, String type){
-        return dbContentsRepository.setQuickMenu(list,type);
-    }
-    public boolean setFloatingMenu(List<FloatingMenuVO> list, String type){
-        return dbContentsRepository.setFloatingMenu(list,type);
+    public boolean setQuickMenu(List<QuickMenuVO> list, String type) {
+        return dbContentsRepository.setQuickMenu(list, type);
     }
 
-    public void setContents(CMN_0002.Response data, ResultCallback callback){
+    public boolean setFloatingMenu(List<FloatingMenuVO> list, String type) {
+        return dbContentsRepository.setFloatingMenu(list, type);
+    }
+
+    public void setContents(CMN_0002.Response data, ResultCallback callback) {
         ExecutorService es = new ExecutorService("");
         Futures.addCallback(es.getListeningExecutorService().submit(() -> {
 
-            boolean isSuccess=false;
+            boolean isSuccess = false;
 
             try {
                 isSuccess = setWeatherList(data.getWthrInsgtList())
-                        &&setQuickMenu(data.getMenu0000().getQckMenuList(), MAIN_VEHICLE_TYPE_0000)
-                        &&setFloatingMenu(data.getMenu0000().getMenuList(), MAIN_VEHICLE_TYPE_0000)
-                        &&setQuickMenu(data.getMenuCV().getQckMenuList(), MAIN_VEHICLE_TYPE_CV)
-                        &&setFloatingMenu(data.getMenuCV().getMenuList(), MAIN_VEHICLE_TYPE_CV)
-                        &&setQuickMenu(data.getMenuNV().getQckMenuList(), MAIN_VEHICLE_TYPE_NV)
-                        &&setFloatingMenu(data.getMenuNV().getMenuList(), MAIN_VEHICLE_TYPE_NV)
-                        &&setQuickMenu(data.getMenuOV().getQckMenuList(), MAIN_VEHICLE_TYPE_OV)
-                        &&setFloatingMenu(data.getMenuOV().getMenuList(), MAIN_VEHICLE_TYPE_OV);
+                        && setQuickMenu(data.getMenu0000().getQckMenuList(), MAIN_VEHICLE_TYPE_0000)
+                        && setFloatingMenu(data.getMenu0000().getMenuList(), MAIN_VEHICLE_TYPE_0000)
+                        && setQuickMenu(data.getMenuCV().getQckMenuList(), MAIN_VEHICLE_TYPE_CV)
+                        && setFloatingMenu(data.getMenuCV().getMenuList(), MAIN_VEHICLE_TYPE_CV)
+                        && setQuickMenu(data.getMenuNV().getQckMenuList(), MAIN_VEHICLE_TYPE_NV)
+                        && setFloatingMenu(data.getMenuNV().getMenuList(), MAIN_VEHICLE_TYPE_NV)
+                        && setQuickMenu(data.getMenuOV().getQckMenuList(), MAIN_VEHICLE_TYPE_OV)
+                        && setFloatingMenu(data.getMenuOV().getMenuList(), MAIN_VEHICLE_TYPE_OV);
             } catch (Exception e1) {
                 e1.printStackTrace();
-                isSuccess=false;
+                isSuccess = false;
             }
             return isSuccess;
         }), new FutureCallback<Boolean>() {
@@ -134,6 +133,41 @@ class CMNViewModel extends ViewModel {
                 es.shutDownExcutor();
             }
         }, es.getUiThreadExecutor());
+    }
+
+
+    public NotiVO getNoti(List<NotiVO> list) throws ExecutionException, InterruptedException {
+        ExecutorService es = new ExecutorService("");
+
+        Future<NotiVO> future = es.getListeningExecutorService().submit(()->{
+            NotiVO notiVO = null;
+
+            try {
+                //NOTI 우선순위 결정
+                for (int i = 0; i < list.size(); i++) {
+                    switch (list.get(i).getNotiCd()) {
+                        case VariableType.NOTI_CODE_EMGR:
+                            notiVO = list.get(i);
+                            break;
+                        case VariableType.NOTI_CODE_ANNC:
+                            if (notiVO == null
+                                    || !notiVO.getNotiCd().equalsIgnoreCase(VariableType.NOTI_CODE_EMGR)) {
+                                notiVO = list.get(i);
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            } catch (Exception ignore) {
+                ignore.printStackTrace();
+                notiVO = null;
+            }
+
+            return notiVO;
+        });
+
+        return future.get();
     }
 
 
--- app/src/main/java/com/genesis/apps/ui/intro/IntroActivity.java
+++ app/src/main/java/com/genesis/apps/ui/intro/IntroActivity.java
@@ -1,8 +1,8 @@
 package com.genesis.apps.ui.intro;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
 import android.text.TextUtils;
 import android.view.View;
 
@@ -10,29 +10,25 @@ import com.genesis.apps.R;
 import com.genesis.apps.comm.model.constants.RequestCodes;
 import com.genesis.apps.comm.model.constants.TestCode;
 import com.genesis.apps.comm.model.constants.VariableType;
-import com.genesis.apps.comm.model.gra.APIInfo;
 import com.genesis.apps.comm.model.gra.APPIAInfo;
 import com.genesis.apps.comm.model.gra.api.CMN_0001;
 import com.genesis.apps.comm.model.gra.api.CMN_0002;
 import com.genesis.apps.comm.model.gra.api.LGN_0001;
 import com.genesis.apps.comm.model.vo.DeviceDTO;
 import com.genesis.apps.comm.model.vo.NotiVO;
-import com.genesis.apps.comm.net.NetUIResponse;
 import com.genesis.apps.comm.util.PackageUtil;
 import com.genesis.apps.comm.viewmodel.CMNViewModel;
 import com.genesis.apps.comm.viewmodel.LGNViewModel;
 import com.genesis.apps.databinding.ActivityIntroBinding;
 import com.genesis.apps.room.ResultCallback;
-import com.genesis.apps.ui.main.MainActivity;
 import com.genesis.apps.ui.common.activity.SubActivity;
 import com.genesis.apps.ui.common.dialog.middle.MiddleDialog;
-import com.google.gson.Gson;
+import com.genesis.apps.ui.main.MainActivity;
 
 import java.util.List;
 
 import javax.inject.Inject;
 
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import dagger.hilt.android.AndroidEntryPoint;
 
@@ -52,6 +48,7 @@ public class IntroActivity extends SubActivity<ActivityIntroBinding> {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+        startProgressTask();
         if(isPermissions()){
             init();
         }
@@ -72,9 +69,20 @@ public class IntroActivity extends SubActivity<ActivityIntroBinding> {
             if (!TextUtils.isEmpty(TestCode.CMN_0001.getNotiDt()))
                 cmnViewModel.updateNotiDt(TestCode.CMN_0001.getNotiDt());
 
-            reqDownloadCarInfo = () -> lgnViewModel.reqLGN0001(new LGN_0001.Request(APPIAInfo.INT01.getId(), PackageUtil.getApplicationVersionName(this, getPackageName()),""));
 
-            reqContentsDownload = () -> cmnViewModel.reqCMN0002(new CMN_0002.Request(APPIAInfo.INT01.getId()));
+            reqDownloadCarInfo = new Runnable() {
+                @Override
+                public void run() {
+                    updateProgressBar(progressValue.VEHICLE.getProgress());
+                    lgnViewModel.reqLGN0001(new LGN_0001.Request(APPIAInfo.INT01.getId(), PackageUtil.getApplicationVersionName(IntroActivity.this, getPackageName()),""));
+                }
+            };
+
+            reqContentsDownload = () -> {
+                updateProgressBar(progressValue.CONTENTS.getProgress());
+                cmnViewModel.reqCMN0002(new CMN_0002.Request(APPIAInfo.INT01.getId()));
+            };
+
 
             Runnable notiCheck = () -> {
                 if (!checkNoti(TestCode.CMN_0001.getNotiList(), reqContentsDownload)) {
@@ -226,14 +234,15 @@ public class IntroActivity extends SubActivity<ActivityIntroBinding> {
     }
 
     private void moveToMain() {
-        new Handler().postDelayed(() -> {
-            if (isPushData()) {
-                startActivity(moveToPush(MainActivity.class));
-            } else {
-                startActivitySingleTop(new Intent(IntroActivity.this, MainActivity.class), 0);
-            }
-            finish();
-        }, 2000);
+        updateProgressBar(progressValue.START.getProgress());
+
+
+        if (isPushData()) {
+            startActivity(moveToPush(MainActivity.class));
+        } else {
+            startActivitySingleTop(new Intent(IntroActivity.this, MainActivity.class), 0);
+        }
+        finish();
     }
 
     @Override
@@ -264,11 +273,13 @@ public class IntroActivity extends SubActivity<ActivityIntroBinding> {
         getDataFromIntent();
         setViewModel();
         setObserver();
+        updateProgressBar(progressValue.INTRO.getProgress());
         cmnViewModel.reqCMN0001(new CMN_0001.Request(APPIAInfo.INT01.getId(), PackageUtil.getApplicationVersionName(this, getPackageName())));
     }
 
 
     private boolean isPermissions() {
+        updateProgressBar(progressValue.PERMISSION.getProgress());
         // 최초에 실행해서 권한 팝업으로 이동
         // 한번 권한 팝업을 받는다.
         // 필수 권한이 없는 경우 다시 권한 동의 팝업으로 이동한다.
@@ -291,6 +302,7 @@ public class IntroActivity extends SubActivity<ActivityIntroBinding> {
 
 
     private boolean checkVersion(String newVersion, String versionType, Runnable runnable) {
+        updateProgressBar(progressValue.VERSION.getProgress());
         boolean needUpdate = false;
 
         if (PackageUtil.versionCompare(PackageUtil.getApplicationVersionName(this, getPackageName()), newVersion) < 0) {
@@ -310,30 +322,11 @@ public class IntroActivity extends SubActivity<ActivityIntroBinding> {
     }
 
     private boolean checkNoti(List<NotiVO> list, Runnable runnable) {
+        updateProgressBar(progressValue.NOTI.getProgress());
         NotiVO notiVO = null;
 
         try {
-            //NOTI 우선순위 결정
-            for (int i = 0; i < list.size(); i++) {
-                switch (list.get(i).getNotiCd()) {
-                    case VariableType.NOTI_CODE_EMGR:
-                        notiVO = list.get(i);
-                        break;
-                    case VariableType.NOTI_CODE_ANNC:
-                        if (notiVO == null
-                                || !notiVO.getNotiCd().equalsIgnoreCase(VariableType.NOTI_CODE_EMGR)) {
-                            notiVO = list.get(i);
-                        }
-                        break;
-                    default:
-//                        if (notiVO == null
-//                                || !notiVO.getNotiCd().equalsIgnoreCase(VariableType.NOTI_CODE_EMGR)
-//                                || !notiVO.getNotiCd().equalsIgnoreCase(VariableType.NOTI_CODE_ANNC)) {
-//                            notiVO = list.get(i);
-//                        }
-                        break;
-                }
-            }
+            notiVO = cmnViewModel.getNoti(list);
         } catch (Exception ignore) {
             ignore.printStackTrace();
         } finally {
@@ -361,6 +354,231 @@ public class IntroActivity extends SubActivity<ActivityIntroBinding> {
         }
 
         return notiVO == null ? false : true;
+
+//        NotiVO notiVO = null;
+//
+//        try {
+//            //NOTI 우선순위 결정
+//            for (int i = 0; i < list.size(); i++) {
+//                switch (list.get(i).getNotiCd()) {
+//                    case VariableType.NOTI_CODE_EMGR:
+//                        notiVO = list.get(i);
+//                        break;
+//                    case VariableType.NOTI_CODE_ANNC:
+//                        if (notiVO == null
+//                                || !notiVO.getNotiCd().equalsIgnoreCase(VariableType.NOTI_CODE_EMGR)) {
+//                            notiVO = list.get(i);
+//                        }
+//                        break;
+//                    default:
+////                        if (notiVO == null
+////                                || !notiVO.getNotiCd().equalsIgnoreCase(VariableType.NOTI_CODE_EMGR)
+////                                || !notiVO.getNotiCd().equalsIgnoreCase(VariableType.NOTI_CODE_ANNC)) {
+////                            notiVO = list.get(i);
+////                        }
+//                        break;
+//                }
+//            }
+//        } catch (Exception ignore) {
+//            ignore.printStackTrace();
+//        } finally {
+//            //노티팝업활성화
+//            if (notiVO != null) {
+//                switch (notiVO.getNotiCd()) {
+//                    case VariableType.NOTI_CODE_EMGR:
+//                        MiddleDialog.dialogNoti(this, new Runnable() {
+//                            @Override
+//                            public void run() {
+//                                finish();
+//                            }
+//                        }, notiVO.getNotiTtl(), notiVO.getNotiCont());
+//
+//                        break;
+//                    case VariableType.NOTI_CODE_ANNC:
+//                        MiddleDialog.dialogNoti(this, runnable, notiVO.getNotiTtl(), notiVO.getNotiCont());
+//                        break;
+//                    default:
+//
+//                        break;
+//
+//                }
+//            }
+//        }
+//
+//        return notiVO == null ? false : true;
     }
+
+
+
+
+
+
+
+
+    private ProgressbarTask progressbarTask = null;
+    private WaitNotify waitNotify;
+    private int progressC = 0;
+    private int progressV =0;
+    private PROGRESS progressValue = PROGRESS.START;
+    private boolean isExit=false;
+
+    private class ProgressbarTask extends AsyncTask<Void, Integer, Integer> {
+        @Override
+        protected Integer doInBackground(Void... voids) {
+
+            try {
+                while (!isCancelled() && progressC < 100) {
+                    Thread.sleep(100);
+                    if(progressC<progressV) {
+                        progressC += 5;
+                        publishProgress(progressC);
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return progressC;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(Integer integer) {
+            super.onPostExecute(integer);
+            waitNotify.mNotify();
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            ui.progress.setProgress(values[0],true);
+            ui.tvProgress.setText(values[0]+"%");
+        }
+
+        @Override
+        protected void onCancelled(Integer integer) {
+            super.onCancelled(integer);
+            waitNotify.mNotify();
+        }
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+        }
+    }
+
+
+    private class moveToActivityTask extends AsyncTask<Void, Integer, Integer> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Integer doInBackground(Void... voids) {
+
+            if(progressbarTask.getStatus()!= Status.FINISHED){
+                waitNotify.mWait();
+            }
+
+            return 0;
+        }
+
+
+        @Override
+        protected void onPostExecute(Integer integer) {
+            super.onPostExecute(integer);
+
+            if(!isExit) {
+                moveToMain();
+            }
+
+        }
+    }
+
+    private class WaitNotify {
+        synchronized public void mWait() {
+            try {
+                wait();
+            } catch (Exception e) {
+            }
+
+        }
+        synchronized public void mNotify() {
+            try {
+                notifyAll();
+            } catch (Exception e) {
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        isExit=true;
+        stopProgressTask();
+        super.onDestroy();
+    }
+
+    public enum PROGRESS{
+        PERMISSION(10,R.string.intro_word_1),
+        INTRO(20, R.string.intro_word_2),
+        VERSION(50,R.string.intro_word_3),
+        NOTI(65,R.string.intro_word_4),
+        CONTENTS(70,R.string.intro_word_5),
+        VEHICLE(85,R.string.intro_word_6),
+        START(100,R.string.intro_word_7);
+
+        private int progress_i;
+        private int progress_message;
+        PROGRESS(final int progress_i, int progress_message) {
+            this.progress_i = progress_i;
+            this.progress_message = progress_message;
+        }
+
+        public int getProgress() {
+            return progress_i;
+        }
+
+        public int getProgress_message(int progress_i){
+            for (PROGRESS progress : PROGRESS.values()) {
+                if (progress_i<=progress.progress_i) {
+                    return progress.progress_message;
+                }
+            }
+
+            return 0;
+        }
+
+        public static PROGRESS find(int progress_i) {
+            for (PROGRESS progress : PROGRESS.values()) {
+                if (progress_i==progress.progress_i) {
+                    return progress;
+                }
+            }
+            return null;
+        }
+    }
+
+    private void startProgressTask() {
+        waitNotify = new WaitNotify();
+        progressbarTask = new ProgressbarTask();
+        progressbarTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+    }
+
+    private void stopProgressTask(){
+        if (progressbarTask != null && progressbarTask.getStatus() != AsyncTask.Status.FINISHED) {
+            progressbarTask.cancel(true);
+        }
+    }
+
+    private void updateProgressBar(int i) {
+        progressV = i;
+        progressValue = PROGRESS.find(i);
+    }
+
 
 }
