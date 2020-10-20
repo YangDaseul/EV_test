f2a7b3d0d6171bcabae41cdffb8075b676689dc8
홍찬오 <co-hong@ksmartech.com>
2020-10-20 21:44:14 +0900

    [fix] 대리운전 신청 입력 창 조정  - 레이아웃 스위칭 안 되는 거 해결  - 교체 대상 범위를 전범위로 잡아버려서 자식 뷰의 depth가 높아졌던 것을 원인으로 추정

--- app/src/main/java/com/genesis/apps/ui/common/activity/ServiceDriveReqActivity.java
+++ app/src/main/java/com/genesis/apps/ui/common/activity/ServiceDriveReqActivity.java
@@ -29,7 +29,6 @@ public class ServiceDriveReqActivity extends SubActivity<ActivityServiceDriveReq
             R.layout.activity_service_drive_req_2,
     };
 
-    private View[] constraintViews;
     private ConstraintSet[] constraintSets = new ConstraintSet[layouts.length];
 
     private EditText.OnEditorActionListener editorActionListener = (textView, actionId, keyEvent) -> {
@@ -75,19 +74,14 @@ public class ServiceDriveReqActivity extends SubActivity<ActivityServiceDriveReq
 
     private void init() {
         //TODO : 차종, 번호판 정보 표시
-        ((TextView)ui.lServiceDriveReqTopPanel.findViewById(R.id.tv_service_drive_req_car_model)).setText("GV80");
-        ((TextView)ui.lServiceDriveReqTopPanel.findViewById(R.id.tv_service_drive_req_car_number)).setText("12너 3456");
+        ((TextView) ui.lServiceDriveReqTopPanel.findViewById(R.id.tv_service_drive_req_car_model)).setText("GV80");
+        ((TextView) ui.lServiceDriveReqTopPanel.findViewById(R.id.tv_service_drive_req_car_number)).setText("12너 3456");
 
         initConstraintSets();
         initView();
     }
 
     private void initConstraintSets() {
-        constraintViews = new View[]{
-                ui.lServiceDriveReqInputFromDetail,
-                ui.lServiceDriveReqInputToDetail
-        };
-
         for (int i = 0; i < layouts.length; ++i) {
             constraintSets[i] = new ConstraintSet();
 
@@ -103,11 +97,11 @@ public class ServiceDriveReqActivity extends SubActivity<ActivityServiceDriveReq
         ui.tietServiceDriveReqInputFromAddressDetail.setOnEditorActionListener(editorActionListener);
 
         //TODO 출발지 검색 버튼 : 지도 액티비티로 가야되는데 테스트 중이므로 바로 결과 나왔다 치고 다음 거 보이도록
-        //TODO : 함수로 독립
+        //TODO : 함수로 독립, onActvityResult()에서 호출
         ui.tvServiceDriveReqSearchFromAddressBtn.setOnClickListener(v -> {
             //onActivityResult()에서 이 값을 세팅하고 다음 게 이미 펼쳐져있도록 하면 될 듯?
             //아니면 펼쳐지는 장면을 onActivityResult()에서 보여주거나(이미 입력된 값을 수정하고왔을 때는 펼치기 이펙트 재생 안 해야겠지?)
-            ui.tvServiceDriveReqSearchFromAddressBtn.setText("출발지 주소가 입력되었음");
+            ui.tvServiceDriveReqSearchFromAddressBtn.setText("지도 액티비티에서 가져온 출발지 주소");
             ui.tvServiceDriveReqSearchFromAddressBtn.setTextAppearance(R.style.ServiceDrive_SearchAddress_HasData);
             ui.tvServiceDriveReqSearchFromAddressBtn.setBackground(getDrawable(R.drawable.ripple_bg_ffffff_stroke_141414));
             ui.ivServiceDriveReqPleaseInputXxx.setText(R.string.service_drive_input_02);
@@ -117,11 +111,11 @@ public class ServiceDriveReqActivity extends SubActivity<ActivityServiceDriveReq
         });
 
         //TODO 도착지 검색 버튼 : 지도 액티비티로 가야되는데 테스트 중이므로 바로 결과 나왔다 치고 다음 거 보이도록
-        //TODO : 함수로 독립
+        //TODO : 함수로 독립, onActvityResult()에서 호출
         ui.tvServiceDriveReqSearchToAddressBtn.setOnClickListener(v -> {
             //onActivityResult()에서 이 값을 세팅하고 다음 게 이미 펼쳐져있도록 하면 될 듯?
             //아니면 펼쳐지는 장면을 onActivityResult()에서 보여주거나(이미 입력된 값을 수정하고왔을 때는 펼치기 이펙트 재생 안 해야겠지?)
-            ui.tvServiceDriveReqSearchToAddressBtn.setText("도착지 주소가 입력되었음");
+            ui.tvServiceDriveReqSearchToAddressBtn.setText("지도 액티비티에서 가져온 도착지 주소");
             ui.tvServiceDriveReqSearchToAddressBtn.setTextAppearance(R.style.ServiceDrive_SearchAddress_HasData);
             ui.tvServiceDriveReqSearchToAddressBtn.setBackground(getDrawable(R.drawable.ripple_bg_ffffff_stroke_141414));
             ui.ivServiceDriveReqPleaseInputXxx.setText(R.string.service_drive_input_04);
@@ -140,15 +134,17 @@ public class ServiceDriveReqActivity extends SubActivity<ActivityServiceDriveReq
     }
 
     /**
-     * 출발지 주소 검색버튼 트랜지션
+     * 도착지 주소 검색버튼 트랜지션
      */
     private void doTransition() {
         //TODO if 조건 수정
-        if (constraintViews[TO].getVisibility() == View.GONE) {
+        if (ui.tvServiceDriveReqToTitle.getVisibility() == View.GONE) {
             Transition changeBounds = new ChangeBounds();
             changeBounds.setInterpolator(new OvershootInterpolator());
             TransitionManager.beginDelayedTransition(ui.lServiceDriveReqContainer);
             constraintSets[TO].applyTo(ui.lServiceDriveReqContainer);
+
+            ui.ivServiceDriveReqPleaseInputXxx.setText(R.string.service_drive_input_03);
         }
     }
 }
