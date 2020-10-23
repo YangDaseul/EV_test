6e61871f570af4479eda7da93e406207b31a11c0
홍찬오 <co-hong@ksmartech.com>
2020-10-23 14:58:44 +0900

    [add] 서비스  - 메인에서 각 항목별 액티비티로 진입하는 코드 추가(현재 테스트코드)  - 정비예약 레이아웃 파일 및 액티비티 클래스 파일 추가

---
+++ app/src/main/java/com/genesis/apps/ui/main/service/MaintenanceReserveActivity.java
@@ -0,0 +1,50 @@
+package com.genesis.apps.ui.main.service;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.genesis.apps.R;
+import com.genesis.apps.databinding.ActivityMaintenanceReserveBinding;
+import com.genesis.apps.databinding.DialogBottomNowOrReserveBinding;
+import com.genesis.apps.ui.common.activity.SubActivity;
+
+public class MaintenanceReserveActivity extends SubActivity<ActivityMaintenanceReserveBinding> {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maintenance_reserve);
+        getDataFromIntent();
+        setViewModel();
+        setObserver();
+        initView();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void getDataFromIntent() {
+
+    }
+
+    @Override
+    public void setViewModel() {
+    }
+
+    @Override
+    public void setObserver() {
+    }
+
+    private void initView() {
+    }
+
+
+    @Override
+    public void onClickCommon(View v) {
+
+    }
+
+}
