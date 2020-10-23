f12b831a819317fe585590a36cdd4adf45af6baf
홍찬오 <co-hong@ksmartech.com>
2020-10-23 16:46:28 +0900

    [modify] 대리운전 신청 버튼에 해당 액티비티 호출 붙임(기능은 아직 미구현).

--- app/src/main/java/com/genesis/apps/ui/main/service/FragmentServiceDrive.java
+++ app/src/main/java/com/genesis/apps/ui/main/service/FragmentServiceDrive.java
@@ -1,5 +1,6 @@
 package com.genesis.apps.ui.main.service;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -9,7 +10,9 @@ import android.view.ViewGroup;
 import androidx.annotation.Nullable;
 
 import com.genesis.apps.R;
+import com.genesis.apps.comm.model.constants.VariableType;
 import com.genesis.apps.databinding.FragmentServiceDriveBinding;
+import com.genesis.apps.ui.common.activity.BaseActivity;
 import com.genesis.apps.ui.common.fragment.SubFragment;
 
 public class FragmentServiceDrive extends SubFragment<FragmentServiceDriveBinding> {
@@ -22,7 +25,14 @@ public class FragmentServiceDrive extends SubFragment<FragmentServiceDriveBindin
         View view = super.setContentView(inflater, R.layout.fragment_service_drive);
         me.setFragment(this);
 
+        setOnSingleClickListener();
+
         return view;
+    }
+
+    //이중 클릭 방지 클릭 리스너 붙이기. 이 프래그먼트는 누를 게 신청 버튼 하나 뿐이라 어댑터가 없으니 여기서 붙인다
+    private void setOnSingleClickListener() {
+        me.tvServiceDriveReqBtn.setOnClickListener(onSingleClickListener);
     }
 
     @Override
@@ -37,6 +47,18 @@ public class FragmentServiceDrive extends SubFragment<FragmentServiceDriveBindin
 
     @Override
     public void onClickCommon(View v) {
+        int id = v.getId();
+        Log.d(TAG, "onClickCommon: view id :" + id);
 
+        switch (id) {
+            //대리운전 신청(이미 신청한 상태이면 그 내용을 보여줌)
+            case R.id.tv_service_drive_req_btn:
+                ((BaseActivity) getActivity()).startActivitySingleTop(new Intent(getActivity(), ServiceDriveReqActivity.class), 0, VariableType.ACTIVITY_TRANSITION_ANIMATION_HORIZONTAL_SLIDE);
+                break;
+
+            default:
+                //do nothing
+                break;
+        }
     }
 }
