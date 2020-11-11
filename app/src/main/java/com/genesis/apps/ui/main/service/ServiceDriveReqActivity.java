cb4320aa3f51ac2db5081efc732392c4df4db9c1
홍찬오 <co-hong@ksmartech.com>
2020-11-11 22:40:42 +0900

    [add] 대리운전 신청 페이지  - 다음 버튼 기능 구현 및 구현예정내용 todo기록.

--- app/src/main/java/com/genesis/apps/ui/main/service/ServiceDriveReqActivity.java
+++ app/src/main/java/com/genesis/apps/ui/main/service/ServiceDriveReqActivity.java
@@ -12,7 +12,6 @@ import android.view.View;
 import android.view.animation.OvershootInterpolator;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.transition.ChangeBounds;
@@ -29,6 +28,7 @@ import com.genesis.apps.comm.model.vo.VehicleVO;
 import com.genesis.apps.comm.util.SnackBarUtil;
 import com.genesis.apps.databinding.ActivityServiceDriveReq1Binding;
 import com.genesis.apps.ui.common.activity.SubActivity;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
 public class ServiceDriveReqActivity extends SubActivity<ActivityServiceDriveReq1Binding> {
@@ -39,11 +39,17 @@ public class ServiceDriveReqActivity extends SubActivity<ActivityServiceDriveReq
 
     private static final int NEXT_BTN_LACK_INPUT = 0;
     private static final int NEXT_BTN_OPEN_TO_ADDRESS = 1;
-    private static final int NEXT_BTN_ASK_PRICE = 2;
-    private static final int NEXT_BTN_REQ_SERVICE = 3;
+    private static final int NEXT_BTN_NEED_TO_ADDRESS = 2;
+    private static final int NEXT_BTN_ASK_PRICE = 3;
+    private static final int NEXT_BTN_REQ_SERVICE = 4;
 
     private VehicleVO mainVehicle;
     private AddressVO addressVO;
+
+    private TextInputLayout fromDetailLayout;
+    private TextInputLayout toDetailLayout;
+    private TextInputEditText fromDetail;
+    private TextInputEditText toDetail;
 
     private final int[] layouts = {
             R.layout.activity_service_drive_req_1,
@@ -96,7 +102,7 @@ public class ServiceDriveReqActivity extends SubActivity<ActivityServiceDriveReq
                 ui.tvServiceDriveReqSearchFromAddressBtn.setBackground(getDrawable(R.drawable.ripple_bg_ffffff_stroke_141414));
                 ui.ivServiceDriveReqPleaseInputXxx.setText(R.string.service_drive_input_02);
                 ui.tvServiceDriveReqFromTitle.setVisibility(View.VISIBLE);
-                ui.lServiceDriveReqInputFromDetail.setVisibility(View.VISIBLE);
+                fromDetailLayout.setVisibility(View.VISIBLE);
                 ui.tvServiceDriveReqNextBtn.setVisibility(View.VISIBLE);
                 break;
 
@@ -110,7 +116,7 @@ public class ServiceDriveReqActivity extends SubActivity<ActivityServiceDriveReq
                 ui.tvServiceDriveReqSearchToAddressBtn.setBackground(getDrawable(R.drawable.ripple_bg_ffffff_stroke_141414));
                 ui.ivServiceDriveReqPleaseInputXxx.setText(R.string.service_drive_input_04);
                 ui.tvServiceDriveReqToTitle.setVisibility(View.VISIBLE);
-                ui.lServiceDriveReqInputToDetail.setVisibility(View.VISIBLE);
+                toDetailLayout.setVisibility(View.VISIBLE);
                 break;
 
             //다음 버튼
@@ -168,9 +174,17 @@ public class ServiceDriveReqActivity extends SubActivity<ActivityServiceDriveReq
     }
 
     private void init() {
+        Log.d(TAG, "init: ");
         //차종, 번호판 정보 표시
+        //todo  : 키보드 올라와있는 동안 안 보여야됨  ui.lServiceDriveReqTopPanel
         ui.lServiceDriveReqTopPanel.tvServiceReqCarModel.setText(mainVehicle.getMdlNm());
         ui.lServiceDriveReqTopPanel.tvServiceReqCarNumber.setText(mainVehicle.getCarRgstNo());
+
+
+        fromDetailLayout = ui.lServiceDriveReqInputFromDetail;
+        toDetailLayout = ui.lServiceDriveReqInputToDetail;
+        fromDetail = ui.tietServiceDriveReqInputFromAddressDetail;
+        toDetail = ui.tietServiceDriveReqInputToAddressDetail;
 
         initConstraintSets();
         setTextListener();
@@ -189,8 +203,8 @@ public class ServiceDriveReqActivity extends SubActivity<ActivityServiceDriveReq
     }
 
     public void setTextListener() {
-        ui.tietServiceDriveReqInputFromAddressDetail.setOnEditorActionListener(editorActionListener);
-        ui.tietServiceDriveReqInputFromAddressDetail.addTextChangedListener(new TextWatcher() {
+        fromDetail.setOnEditorActionListener(editorActionListener);
+        fromDetail.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
                 //do nothing
@@ -203,12 +217,12 @@ public class ServiceDriveReqActivity extends SubActivity<ActivityServiceDriveReq
 
             @Override
             public void afterTextChanged(Editable editable) {
-                setError(ui.lServiceDriveReqInputFromDetail, !hasInput(editable.toString()));
+                setError(fromDetailLayout, hasInput(editable.toString()));
             }
         });
 
-        ui.tietServiceDriveReqInputToAddressDetail.setOnEditorActionListener(editorActionListener);
-        ui.tietServiceDriveReqInputToAddressDetail.addTextChangedListener(new TextWatcher() {
+        toDetail.setOnEditorActionListener(editorActionListener);
+        toDetail.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
                 //do nothing
@@ -221,7 +235,7 @@ public class ServiceDriveReqActivity extends SubActivity<ActivityServiceDriveReq
 
             @Override
             public void afterTextChanged(Editable editable) {
-                setError(ui.lServiceDriveReqInputToDetail, !hasInput(editable.toString()));
+                setError(toDetailLayout, hasInput(editable.toString()));
             }
         });
     }
@@ -232,36 +246,104 @@ public class ServiceDriveReqActivity extends SubActivity<ActivityServiceDriveReq
     }
 
     //에러 메시지 표시하기/끄기
-    private void setError(TextInputLayout textInputLayout, boolean enable) {
-        if (enable) {
-            textInputLayout.setError(getString(R.string.service_drive_input_00));
-        } else {
+    private void setError(TextInputLayout textInputLayout, boolean hasData) {
+        if (hasData) {
             textInputLayout.setError(null);
+        } else {
+            textInputLayout.setError(getString(R.string.service_drive_input_00));
         }
     }
 
     private void onClickNextBtn() {
-        determineWork();
+        int work = determineWork();
+        Log.d(TAG, "onClickNextBtn: " + work);
 
-        //출발지 상세주소 입력 완료하면 도착지 검색버튼 뜨도록 리스너 붙임(화면 맨 아래 다음 버튼)
-        if (getCurrentFocus() instanceof TextView) {
-            if (!TextUtils.isEmpty(((TextView) getCurrentFocus()).getText().toString())) {
+        switch (work) {
+
+            case NEXT_BTN_OPEN_TO_ADDRESS:
                 doTransition();
+                //not break
+            case NEXT_BTN_NEED_TO_ADDRESS:
+                //todo 도착지 주소검색 버튼 누름
+                //todo 키보드 숨김처리 명시적으로 필요한지 확인하여 반영
+                break;
 
-            }
+            case NEXT_BTN_ASK_PRICE:
+                //todo [1] 서버에 예상 가격 물어보기
+                // 그동안 로딩 뷰스텁 띄우기(점 세 개짜리).
+                // 로딩하는 동안 다음버튼 비활성화
+
+                // 예상가격 받으면 저장. 다음 버튼 재활성화
+                // 서비스 불가 지역 : 다음 비활성 및 뷰스텁 갱신
+                // 오류 : 다음 비활성 및 재시도 뷰스텁 재시도하면 [1]을 다시 실행
+                //
+
+
+                break;
+
+            case NEXT_BTN_REQ_SERVICE:
+                //todo : 지금할래, 예약할래 대화상자 호출
+                // 만들어야 됨... 레이아웃만 있음
+                // R.layout.dialog_bottom_now_or_reserve 보드 167쪽
+
+                break;
+
+            case NEXT_BTN_LACK_INPUT:   //입력 미비 todo 디폴트랑 분리할지 고민
+            default:
+                //do nothing
+                break;
         }
+
 
     }
 
     //입력 상태를 검사하여 "다음" 버튼이 처리할 내용 결정
     private int determineWork() {
+        Log.d(TAG, "determineWork: ");
 
+        boolean hasFromDetail = hasInput(fromDetail.getText().toString());
+        boolean hasToDetail = hasInput(toDetail.getText().toString());
+
+        //세부 주소 빈칸인지 검사
+        checkDetailAddress(hasFromDetail, hasToDetail);
+
+        //도착지 검색 버튼 해금 :
+        //버튼이 안 보이고 출발 상세주소가 입력된 상태
+        if (ui.tvServiceDriveReqSearchToAddressBtn.getVisibility() == View.GONE && hasFromDetail) {
+            return NEXT_BTN_OPEN_TO_ADDRESS;
+        }
+
+        //도착지 주소 안 고르고 뒤로 와버려서
+        // 도착지 주소 버튼이 기본값으로 보이는 경우
+        if (ui.tvServiceDriveReqSearchToAddressBtn.getVisibility() == View.VISIBLE &&
+                ui.tvServiceDriveReqSearchToAddressBtn.getText().toString().equals(getString(R.string.service_drive_input_09))) {
+            return NEXT_BTN_NEED_TO_ADDRESS;
+        }
+
+        //세부 주소 입력 미비 : 여기서부터는 [[입력이 하나라도 없으면]] 바로 리턴해버림
+        if (!hasFromDetail || !hasToDetail) {
+            //todo 저장해둔 예상가격이 있으면 삭제 : 예상가격 보고 나서 세부주소를 지웠다는 뜻임. 여기서 해야되나, 리턴 받은 뒤에 해도 되나 고민
+            return NEXT_BTN_LACK_INPUT;
+        }
+
+        //여기까지 왔으면 입력은 다 됐음
+
+        //if(가격을 모른다)
         return NEXT_BTN_ASK_PRICE;
 
+        //예상 가격 값이 있다
+        //todo
+//        return NEXT_BTN_REQ_SERVICE;
+    }
 
-//        NEXT_BTN_LACK_INPUT;
-//        NEXT_BTN_OPEN_TO_ADDRESS;
-//        NEXT_BTN_REQ_SERVICE;
+    private void checkDetailAddress(boolean hasFromDetail, boolean hasToDetail) {
+        //출발지 세부 주소가 빈칸인 채로 진행하려고 하는 경우(입력 창에 손도 안 댄 경우) 입력 창에 에러메시지 출력
+        setError(fromDetailLayout, hasFromDetail);
+
+        //도착지 세부 주소는 입력 창이 해금된 상태에서만 검사
+        if (toDetailLayout.getVisibility() == View.VISIBLE) {
+            setError(toDetailLayout, hasToDetail);
+        }
     }
 
     /**
