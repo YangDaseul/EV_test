51a23b86e31bc993f7488212ab363a58c417ccc2
jo <jojojo7827@ksmartech.com>
2021-01-25 14:58:22 +0900

    [modify] 프리빌리지 차량 목록 UI 수정

--- app/src/main/java/com/genesis/apps/ui/myg/view/PrivilegeApplyAdapter.java
+++ app/src/main/java/com/genesis/apps/ui/myg/view/PrivilegeApplyAdapter.java
@@ -47,34 +47,34 @@ public class PrivilegeApplyAdapter extends BaseRecyclerViewAdapter2<PrivilegeVO>
         public void onBindView(PrivilegeVO item, int pos) {
             getBinding().btnApply.setOnClickListener(null);
             getBinding().btnApply.setVisibility(View.GONE);
-            getBinding().btnBenefit.setOnClickListener(null);
-            getBinding().btnBenefit.setVisibility(View.GONE);
+//            getBinding().btnBenefit.setOnClickListener(null);
+//            getBinding().btnBenefit.setVisibility(View.GONE);
             getBinding().btnStatus.setOnClickListener(null);
             getBinding().btnStatus.setVisibility(View.GONE);
 
             switch (item.getJoinPsblCd()){
                 case PrivilegeVO.JOIN_CODE_APPLY_POSSIBLE:
-                    getBinding().lWhole.setBackgroundResource(R.drawable.bg_ffffff_stroke_e2e2e2);
+                    getBinding().lWhole.setBackgroundResource(R.drawable.bg_ffffff_stroke_cd9a81);
                     getBinding().btnStatus.setVisibility(View.GONE);
-                    getBinding().btnBenefit.setVisibility(View.GONE);
+//                    getBinding().btnBenefit.setVisibility(View.GONE);
                     getBinding().btnApply.setVisibility(View.VISIBLE);
                     getBinding().btnApply.setTag(R.id.url, item.getServiceUrl());
                     getBinding().btnApply.setOnClickListener(onSingleClickListener);
                     break;
                 case PrivilegeVO.JOIN_CODE_APPLYED:
-                    getBinding().lWhole.setBackgroundResource(R.drawable.bg_ffffff_stroke_cd9a81);
+                    getBinding().lWhole.setBackgroundResource(R.drawable.bg_ffffff_stroke_e5e5e5);
                     getBinding().btnApply.setVisibility(View.GONE);
                     getBinding().btnStatus.setVisibility(View.VISIBLE);
-                    getBinding().btnBenefit.setVisibility(View.VISIBLE);
+//                    getBinding().btnBenefit.setVisibility(View.VISIBLE);
                     getBinding().btnStatus.setTag(R.id.url, item.getServiceUrl());
-                    getBinding().btnBenefit.setTag(R.id.url, item.getServiceDetailUrl());
+//                    getBinding().btnBenefit.setTag(R.id.url, item.getServiceDetailUrl());
                     getBinding().btnStatus.setOnClickListener(onSingleClickListener);
-                    getBinding().btnBenefit.setOnClickListener(onSingleClickListener);
+//                    getBinding().btnBenefit.setOnClickListener(onSingleClickListener);
                     break;
             }
 
             getBinding().tvModel.setText(item.getMdlNm());
-            getBinding().tvCarRgstNo.setText(item.getSaleMdlNm());
+            getBinding().tvEngine.setText(item.getSaleMdlNm());
             getBinding().tvVin.setText(item.getVin());
         }
 
