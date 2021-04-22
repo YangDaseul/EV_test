3a535c877f57f6fc297e472ad54dd2cb25132314
hjpark <hjpark@ksmartech.com>
2021-04-22 14:54:56 +0900

    [추가] 충전소 예약 내역 작업 중

--- app/src/main/java/com/genesis/apps/ui/main/service/ChargeReserveHistoryActivity.java
+++ app/src/main/java/com/genesis/apps/ui/main/service/ChargeReserveHistoryActivity.java
@@ -1,35 +1,32 @@
 package com.genesis.apps.ui.main.service;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
 
-import androidx.databinding.DataBindingUtil;
-import androidx.viewpager2.widget.ViewPager2;
-
 import com.genesis.apps.R;
-import com.genesis.apps.comm.model.constants.KeyNames;
-import com.genesis.apps.databinding.ActivityServiceRepairHistoryBinding;
-import com.genesis.apps.databinding.ItemTabRepairImageBinding;
+import com.genesis.apps.comm.model.vo.VehicleVO;
+import com.genesis.apps.comm.viewmodel.REQViewModel;
+import com.genesis.apps.databinding.ActivityChargeReserveHistoryBinding;
 import com.genesis.apps.ui.common.activity.SubActivity;
 import com.genesis.apps.ui.main.service.view.ServiceRepairHistoryViewpagerAdapter;
-import com.google.android.material.tabs.TabLayoutMediator;
+import com.genesis.apps.ui.main.service.view.ServiceRepairReserveHistoryAdapter;
 
 /**
  * @author hjpark
- * @brief 충전소 에약 내역
+ * @brief 충전소 예약 내역
  */
-public class ChargeReserveHistoryActivity extends SubActivity<ActivityServiceRepairHistoryBinding> {
+public class ChargeReserveHistoryActivity extends SubActivity<ActivityChargeReserveHistoryBinding> {
 
-    private ServiceRepairHistoryViewpagerAdapter serviceRepairHistoryViewpagerAdapter;
-    private final int PAGE_NUM = 2;//현황예약, 이력
-    private final int[] TAB_ID_LIST = {R.string.sm_r_rsv05_2, R.string.sm_r_rsv05_3};
-    private boolean isTwo;
+    private static final int PAGE_SIZE = 20;
+    private ServiceRepairReserveHistoryAdapter adapter;
+    private REQViewModel reqViewModel;
+    private VehicleVO mainVehicle;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_service_repair_history);
+        setContentView(R.layout.activity_charge_reserve_history);
         getDataFromIntent();
         setViewModel();
         setObserver();
@@ -37,39 +34,9 @@ public class ChargeReserveHistoryActivity extends SubActivity<ActivityServiceRep
     }
 
     private void initView() {
-        serviceRepairHistoryViewpagerAdapter = new ServiceRepairHistoryViewpagerAdapter(this, PAGE_NUM);
-        ui.vpContents.setAdapter(serviceRepairHistoryViewpagerAdapter);
-        ui.vpContents.setUserInputEnabled(false);
-        ui.vpContents.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);
-        ui.vpContents.setCurrentItem(0);
-        ui.vpContents.setOffscreenPageLimit(PAGE_NUM);
-        initTabView();
     }
 
     private void initTabView() {
-        new TabLayoutMediator(ui.tlTabs, ui.vpContents, (tab, position) -> {
-        }).attach();
-
-//        for (int i = 0; i < PAGE_NUM; i++) {
-//            TextView tabTitle = new TextView(this);
-//            tabTitle.setText(TAB_ID_LIST[i]);
-//            tabTitle.setTextAppearance(R.style.ServiceMainTabs);
-//            ui.tlTabs.getTabAt(i).setCustomView(tabTitle);
-//        }
-
-        for(int i=0 ; i<TAB_ID_LIST.length; i++) {
-            final LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            final ItemTabRepairImageBinding itemTabRepairImageBinding = DataBindingUtil.inflate(inflater, R.layout.item_tab_repair_image, null, false);
-            final View view = itemTabRepairImageBinding.getRoot();
-            itemTabRepairImageBinding.tvTab.setText(TAB_ID_LIST[i]);
-            ui.tlTabs.getTabAt(i).setCustomView(view);
-        }
-
-
-        if(isTwo){
-            isTwo=false;
-            ui.tlTabs.getTabAt(1).view.performClick();
-        }
     }
 
     @Override
---
+++ app/src/main/java/com/genesis/apps/ui/main/service/view/ChargeReserveHistoryAdapter.java
@@ -0,0 +1,69 @@
+package com.genesis.apps.ui.main.service.view;
+
+import android.util.SparseBooleanArray;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.genesis.apps.R;
+import com.genesis.apps.comm.model.vo.RepairHistVO;
+import com.genesis.apps.databinding.ItemServiceRepairHistoryBinding;
+import com.genesis.apps.ui.common.view.listener.OnSingleClickListener;
+import com.genesis.apps.ui.common.view.listview.BaseRecyclerViewAdapter2;
+import com.genesis.apps.ui.common.view.viewholder.BaseViewHolder;
+
+
+public class ChargeReserveHistoryAdapter extends BaseRecyclerViewAdapter2<RepairHistVO> {
+
+    private int pageNo = 0;
+    private OnSingleClickListener onSingleClickListener;
+
+    public ChargeReserveHistoryAdapter(OnSingleClickListener onSingleClickListener) {
+        this.onSingleClickListener = onSingleClickListener;
+    }
+
+    @Override
+    public BaseViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ItemServiceRepairHistory(getView(parent, R.layout.item_service_repair_history), onSingleClickListener);
+    }
+
+    @Override
+    public void onBindViewHolder(final BaseViewHolder holder, int position) {
+        holder.onBindView(getItem(position), position);
+    }
+
+    public int getPageNo() {
+        return pageNo;
+    }
+
+    public void setPageNo(int pageNo) {
+        this.pageNo = pageNo;
+    }
+
+    private static class ItemServiceRepairHistory extends BaseViewHolder<RepairHistVO, ItemServiceRepairHistoryBinding> {
+        private OnSingleClickListener onSingleClickListener;
+        public ItemServiceRepairHistory(View itemView, OnSingleClickListener onSingleClickListener) {
+            super(itemView);
+            this.onSingleClickListener = onSingleClickListener;
+        }
+
+        @Override
+        public void onBindView(RepairHistVO item) {
+
+        }
+
+        @Override
+        public void onBindView(RepairHistVO item, int pos) {
+            getBinding().setData(item);
+            getBinding().setPos(pos);
+            getBinding().setListener(onSingleClickListener);
+            getBinding().btnRepairImage.setTag(R.id.item, item);
+        }
+
+        @Override
+        public void onBindView(RepairHistVO item, int pos, SparseBooleanArray selectedItems) {
+
+        }
+
+    }
+
+}
