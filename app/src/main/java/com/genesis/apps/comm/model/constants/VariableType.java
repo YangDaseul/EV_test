3ba87a77e8136a9dea42ecc611ad71624424e65a
hjpark <hjpark@ksmartech.com>
2021-04-27 00:50:55 +0900

    [추가] 차량상세 모빌리티케어 쿠폰 EV 요건 반영  - EV차량일 경우 엔진/오일, 브레이크오일 비활성화  - 내연기관차량일 경우 냉각수 비활성화 (냉각수 코드번호 확인 필요)

--- app/src/main/java/com/genesis/apps/comm/model/constants/VariableType.java
+++ app/src/main/java/com/genesis/apps/comm/model/constants/VariableType.java
@@ -201,6 +201,7 @@ public class VariableType {
     public static final String COUPON_CODE_WIPER = "33";//와이퍼블레이드
     public static final String COUPON_CODE_BREAK_OIL = "34";//브레이크오일
     public static final String COUPON_CODE_PICKUP_DELIVERY = "61";//픽업앤딜리버리
+    public static final String COUPON_CODE_ICE = "66";//냉각수 //TODO 냉각수 코드 변경 필요
     public static final String COUPON_CODE_SONAKS = "99";//프리미엄 소낙스 세차 이용권
 
     //버틀러 상담유형 코드
