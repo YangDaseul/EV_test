f0a93d2017f891cafdaa9bc18be47aa5bb28a5f4
hjpark <hjpark@ksmartech.com>
2021-04-27 00:30:07 +0900

    [추가] 홈-하단 소모품 현황 EV 요건 반영  - EV차량일 경우 엔진/오일 비활성화  - 내연기관차량일 경우 냉각수 비활성화 (냉각수 코드번호 확인 필요)

--- app/src/main/java/com/genesis/apps/comm/model/vo/developers/SestVO.java
+++ app/src/main/java/com/genesis/apps/comm/model/vo/developers/SestVO.java
@@ -12,6 +12,12 @@ import lombok.EqualsAndHashCode;
  * @brief 소모품 리스트
  * @author hjpark
  * @see #sestCode 소모품 코드
+ * 1 : 엔진오일/필터
+ * 2 : 에어클리너
+ * 3 : 에어컨 필터
+ * 4 : 브레이크 패드
+ * 9 : 브레이크 오일
+ * ? : 냉각수
  * @see #sestName 소모품명
  * @see #stdDistance 교환 표준 주행 거리(단위: km)
  * @see #lastInfo 최종 교환 정보
